67fbf7374f3ac0310c90fe8c1c49f6c362a5c95c
Jhon <jhon.corzo.2020@upb.edu.co>
2021-11-08 18:13:08 -0500

    Reutilizacion del algoritmo mergesort, utilizado en ejercicios pasados, y modificado para utilizarlo con los contenedores.

--- src/Punto2/Container.java
+++ src/Punto2/Container.java
@@ -1,4 +1,22 @@
 package Punto2;
 
 public class Container {
+    private int peso;
+    private String name;
+
+    public Container(String name, int peso){
+        this.peso = peso;
+        this.name = name;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+    public String getName(){
+        return name;
+    }
+    @Override
+    public String toString() {
+        return "Container#" + name + ", peso: " + peso;
+    }
 }
---
+++ src/Punto2/MergeSort.java
@@ -0,0 +1,54 @@
+package Punto2;
+
+public class MergeSort {
+    public static void merge(Container arr[], int l, int m, int r){
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        //Arrays temporales
+        Container L[] = new Container[n1];
+        Container R[] = new Container[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        int i = 0, j = 0;
+
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i].getPeso() <= R[j].getPeso()) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    //Ordena con por medio del merge
+    public static void sort(Container arr[], int l, int r)
+    {
+        if (l < r) {
+            int m =l+ (r-l)/2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+}
