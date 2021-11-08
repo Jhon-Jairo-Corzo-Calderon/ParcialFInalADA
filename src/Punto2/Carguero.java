2eddee0d0b7e783060e5b5923cc2c0f23607197c
Jhon <jhon.corzo.2020@upb.edu.co>
2021-11-08 18:15:58 -0500

    Clase carguero codificada, con los atributos y metodos considerados necesarios para resolver el problema.

--- src/Punto2/Carguero.java
+++ src/Punto2/Carguero.java
@@ -1,4 +1,39 @@
 package Punto2;
 
+import java.util.ArrayList;
+
 public class Carguero {
+    private int capCarga;
+    private int lsContainer;
+    private ArrayList<Object[]> carga;
+
+    public Carguero(int capCarga){
+        this.capCarga = capCarga;
+        this.carga = new  ArrayList<Object[]>();
+        lsContainer = 0;
+    }
+
+    public int getCapCarga() {
+        return capCarga;
+    }
+    public ArrayList<Object[]> getCarga(){
+        return carga;
+    }
+
+    public int getCantContainer(){
+        return carga.size();
+    }
+
+    public void addContainer(Container cr, int cant){
+        carga.add(new Object[]{cant,cr});
+    }
+
+    public String stringCarga(){
+        String output = "Contenedores cargados: \n";
+        for (Object[] ob: carga) {
+            if((int) ob[0]!=0)
+                output += ob[0] + " contenedores de #" + (((Container) ob[1]).getName()) +" y peso "+ (((Container) ob[1]).getPeso()) +".\n";
+        }
+        return output;
+    }
 }
